80883d3320abe7873e2e7767431b73e93b08fe2c
Eduardo Santana Santos <eduardo.ssantos@sptech.school>
2026-05-28 19:20:01 -0300

    Finalizando game.

--- fase1.js
+++ fase1.js
@@ -15,6 +15,36 @@ let fase1Quiz3 = {
     questoes:['Cooler','Registradores','Placa-mãe','GPU'],
     qCerta:1
 }
+let fase1Quiz4 = {
+    pergunta:'Quais são os tipos de memórias de um computado?',
+    questoes:['RAM, ROM, Eprom, Flash, memória de massa','Memória interna e externa','Memórias em nuvem e local','SSD E HDD'],
+    qCerta:0
+}
+let fase1Quiz5 = {
+    pergunta:'Qual tecnologia permite acessar a memoria RAM sem passar pela CPU?',
+    questoes:['LED','Direct X','IA','DMA'],
+    qCerta:3
+}
+let fase1Quiz6 = {
+    pergunta:'Qual a sigla certa para Chip select?',
+    questoes:['SelectC','SC','CS','ChipS'],
+    qCerta:2
+}
+let fase1Quiz7 = {
+    pergunta:'Quais dessas opções contem apenas barramentos?',
+    questoes:['adress bus e CPU','Adress bus e Data bus','Cobre e Ferro','Process BUS'],
+    qCerta:1
+}
+let fase1Quiz8 = {
+    pergunta:'Quais desse processadores são da Intel',
+    questoes:['I5 e I7','Ryzen 5 Ryzen 9','CPUX','CpuIntel'],
+    qCerta:0
+}
+let fase1Quiz9 = {
+    pergunta:'Quais os nomes do processadores que tem 2 e 4 nucleos',
+    questoes:['Dual N e squad N','I2 e I4','2N e 4N','dual core e quad core'],
+    qCerta:1
+}
     
     let listaObstaculoFase1 = [
         {tipo:'inimigo' ,top: '0px', left: '150px', width: '250px', height: '200px' },
@@ -23,16 +53,21 @@ let fase1Quiz3 = {
         {tipo:'inimigo' ,top: '300px', left: '600px', width: '200px', height: '250px' },
         {tipo:'inimigo' ,top: '300px', left: '0px', width: '400px', height: '4400px' },
         {tipo:'inimigo' ,top: '150px', left: '450px', width: '150px', height: '400px' },
-        {tipo:'inimigo' ,top: '600px', left: '450px', width: '350px', height: '60px' },
+        {tipo:'inimigo' ,top: '610px', left: '450px', width: '350px', height: '50px' },
         {tipo:'inimigo' ,top: '380px', left: '850px', width: '240px', height: '2500px' },
         {tipo:'inimigo' ,top: '270px', left: '1090px', width: '190px', height: '30px' },
         {tipo:'inimigo' ,top: '270px', left: '1050px', width: '40px', height: '200px' },
         {tipo:'inimigo' ,top: '000px', left: '730px', width: '270px', height: '340px' },
         {tipo:'inimigo' ,top: '000px', left: '1000px', width: '270px', height: '100px' },
-        {tipo:'inimigo' ,top: '130px', left: '1100px', width: '140px', height: '100px' },
-        {tipo:'quiz',quiz:1,quizInfo:fase1Quiz1 ,top: '320px', left: '1150px', width: '120px', height: '120px' },
-        {tipo:'quiz',quiz:2,quizInfo:fase1Quiz2 ,top: '460px', left: '1150px', width: '120px', height: '120px' },
-        {tipo:'quiz',quiz:3,quizInfo:fase1Quiz3 ,top: '600px', left: '1150px', width: '120px', height: '120px' },
-
+        {tipo:'inimigo' ,top: '100px', left: '1100px', width: '140px', height: '100px' },
+        {tipo:'quiz',quiz:1,quizInfo:fase1Quiz1 ,top: '200px', left: '150px', width: '100px', height: '100px' },
+        {tipo:'quiz',quiz:2,quizInfo:fase1Quiz2 ,top: '190px', left: '599px', width: '62px', height: '62px' },
+        {tipo:'quiz',quiz:3,quizInfo:fase1Quiz3 ,top: '665px', left: '570px', width: '60px', height: '60px' },
+        {tipo:'quiz',quiz:4,quizInfo:fase1Quiz4 ,top: '553px', left: '570px', width: '57px', height: '57px' },
+        {tipo:'quiz',quiz:5,quizInfo:fase1Quiz5 ,top: '400px', left: '798px', width: '55px', height: '55px' },
+        {tipo:'quiz',quiz:6,quizInfo:fase1Quiz6 ,top: '110px', left: '1020px', width: '55px', height: '55px' },
+        {tipo:'quiz',quiz:7,quizInfo:fase1Quiz7 ,top: '210px', left: '1120px', width: '55px', height: '55px' },
+        {tipo:'quiz',quiz:8,quizInfo:fase1Quiz8 ,top: '330px', left: '1150px', width: '120px', height: '120px' },
+        {tipo:'quiz',quiz:9,quizInfo:fase1Quiz9 ,top: '550px', left: '1150px', width: '120px', height: '120px' },
     ]
 
--- logicaQuiz.js
+++ logicaQuiz.js
@@ -1,6 +1,7 @@
 const letras = ["A", "B", "C", "D"];
 let respondidos = 0;
 function carregarQuiz(quiz, num) {
+  liberado = false 
   modalQuiz.style = "display:flex; opacity:1";
   let htmlQuestoes = "";
   for (let i = 0; i < quiz.questoes.length; i++) {
@@ -28,26 +29,25 @@ function selecionar(correto, numero) {
     respondidos++;
     modalQuiz.style = "display:none; opacity:0";
     document.getElementById(`quizCirculo${numero}`).style.display = "none";
-    let lista = [];
-    if (fase == 1) {
-      lista = listaObstaculoFase1;
-    }else if(fase == 2){
-        
-    }
+    let lista = listaObstaculoFase1;
     for (let i = 0; i < lista.length; i++) {
       let atual = lista[i];
       if (atual.tipo == "quiz") {
         if (atual.quiz == numero) {
-          if (fase == 1) {
+          
              listaObstaculoFase1[i].top =999999999;
-          }
+          
         }
       }
     }
-    if (respondidos == 3) {
-      alert("proxima fase");
+    if (respondidos == 9) {
+      MsgContainer.style.display = 'flex'
+      
+      
+
     }
   } else {
     window.location.reload();
   }
+  liberado = true
 }
